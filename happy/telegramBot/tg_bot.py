09c28f535c47fa180d985fb3ae9208f18447aec3
tvizzi11k <114189192+tvizzi11k@users.noreply.github.com>
2024-05-02 13:59:50 +0300

    a database has been made, as well as a test bot has been made

---
+++ happy/telegramBot/tg_bot.py
@@ -0,0 +1,21 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = '7120451302:AAFmiJriqQlj0aDohxsBjIRdTQbbJOMEyhs'
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard =[
+        [InlineKeyboardButton("голые сиськи", web_app=WebAppInfo(url="https://www.youtube.com/watch?v=Cxmvq1MCR3c"))]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("порно видео секс", reply_markup=reply_markup)
+
+def setup_bot():
+    application = Application.builder().token(TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    return application
+
+app = setup_bot()
+
+if __name__ == '__main__':
+    app.run_polling()
