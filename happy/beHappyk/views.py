aa0da169527cfeb63767fe0003d95e8c43e8aeb9
tvizzi11k <114189192+tvizzi11k@users.noreply.github.com>
2024-05-03 20:33:07 +0300

    pipiska

--- happy/beHappyk/views.py
+++ happy/beHappyk/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User, Promocode
+from django.contrib import messages
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def home(request):
+    if request.method == 'POST':
+        promocode_input = request.POST.get('promocode')
+        try:
+            promocode = Promocode.objects.get(code=promocode_input)
+            if promocode.used:
+                messages.error(request, "Промокод использован")
+            else:
+                user = request.user
+                user.balance += promocode.value
+                user.save()
+                promocode.used = True
+                promocode.save()
+                messages.success(request, f"Промокод на сумму {promocode.value} применен")
+        except Promocode.DoesNotExist:
+            messages.error(request, "Промокода не существует.")
+    
+    return render(request, 'index.html', {'message': messages.get_messages(request)})
--- happy/happy/settings.py
+++ happy/happy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-gl^$9%dlkv56!#g)_r_8nz@)o!tfdwny)+r2)$i56ap6zz)hg=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'happy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR.parent, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,7 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR.parent, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
