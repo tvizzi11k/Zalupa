2c8c79edcd21612031c0545bf64056e3f675ac75
Danil <94139390+dajeo@users.noreply.github.com>
2024-05-12 03:45:51 +0500

    fix views

--- megahappy/beHappyk/views.py
+++ megahappy/beHappyk/views.py
@@ -1,16 +1,4 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth import login
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_protect  # Используем защиту CSRF
-from .models import User, Promocode
-from django.http import JsonResponse, HttpResponse
-import json
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login, authenticate
-import requests
-from django.contrib.auth.forms import UserCreationForma
-
+from django.shortcuts import render
 
 async def home(request):
     context = {}
