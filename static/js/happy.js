744fad77e9324c2c011beea1bce3062b3602e188
Danil <94139390+dajeo@users.noreply.github.com>
2024-05-12 17:31:00 +0500

    fix front

--- static/js/happy.js
+++ static/js/happy.js
@@ -1,4 +1,4 @@
 const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
-    manifestUrl: 'https://176-99-11-185.cloudvps.regruhosting.ru//tonconnect-manifest.json',
+    manifestUrl: 'https://176-99-11-185.cloudvps.regruhosting.ru/static/ton.json',
     buttonRootId: 'ton-connect'
 });
