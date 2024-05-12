4986827565431c5bbb23e60bccb13bb8f5122825
Daniil <detromixxan@gmail.com>
2024-05-12 15:23:08 +0300

    ton-connection btn

--- static/js/happy.js
+++ static/js/happy.js
@@ -1,1 +1,4 @@
- 
+const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
+    manifestUrl: 'https://176-99-11-185.cloudvps.regruhosting.ru//tonconnect-manifest.json',
+    buttonRootId: 'ton-connect'
+});
