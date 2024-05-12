e1eb0a10c0acbd4a066deb0081516fbbaf260dd4
Danil <94139390+dajeo@users.noreply.github.com>
2024-05-12 18:16:20 +0500

    fix redirects

--- static/js/happy.js
+++ static/js/happy.js
@@ -2,3 +2,7 @@ const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
     manifestUrl: 'https://176-99-11-185.cloudvps.regruhosting.ru/static/ton.json',
     buttonRootId: 'ton-connect'
 });
+
+const unsubscribe = tonConnectUI.onStatusChange(state => {
+    window.location.replace('/home')
+});
--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,8 @@
 Autorization()
 
 function Autorization(){
-  if (localStorage.getItem('ton-connect-storage_bridge-connection') !== null) {
-    window.location.href = 'https://176-99-11-185.cloudvps.regruhosting.ru/home';
-  } else {
-    window.location.href = 'https://176-99-11-185.cloudvps.regruhosting.ru/';
-  }  
+  if (localStorage.getItem('ton-connect-storage_bridge-connection') === null) {
+    window.location.replace('/')
+  }
 }
 
