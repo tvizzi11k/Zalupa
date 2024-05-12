7573fd0654f4f08541644b6d72231e00363b121c
Daniil <detromixxan@gmail.com>
2024-05-12 18:14:09 +0300

    update cnt-btn

--- static/js/happy.js
+++ static/js/happy.js
@@ -1,8 +1,14 @@
 const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
     manifestUrl: 'https://176-99-11-185.cloudvps.regruhosting.ru/static/ton.json',
-    buttonRootId: 'ton-connect'
+    buttonRootId: 'ton-connect',
 });
 
 const unsubscribe = tonConnectUI.onStatusChange(state => {
     window.location.replace('/home')
 });
+
+// tonConnectUI.uiOptions = {
+//   uiPreferences: {
+//     borderRadius: 'm'
+// }
+// };
