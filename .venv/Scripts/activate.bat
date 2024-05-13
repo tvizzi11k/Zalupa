65e69206ee41b14e8d94583a51be865f1b32ae26
tvizzi11k <114189192+tvizzi11k@users.noreply.github.com>
2024-05-13 23:08:14 +0300

    change settings req

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\beHappyProj\Zalupa\.venv
+set VIRTUAL_ENV=c:\Users\zgalk\OneDrive\Документы\GitHub\Zalupa\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- megahappy/telegramBot/tg_bot.py
+++ megahappy/telegramBot/tg_bot.py
@@ -9,6 +9,7 @@ from aiogram.types import Message, WebAppInfo
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
+
 TOKEN = '7120451302:AAFmiJriqQlj0aDohxsBjIRdTQbbJOMEyhs'
 
 logger = logging.getLogger(__file__)
