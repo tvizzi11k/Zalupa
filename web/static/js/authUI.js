d35dd4c222a3743b3c27758c1c4cd68187025b19
Daniil <detromixxan@gmail.com>
2024-05-22 00:42:55 +0300

    Update disconnectc btn

--- web/static/js/authUI.js
+++ web/static/js/authUI.js
@@ -1,9 +1,6 @@
 const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
   manifestUrl: 'https://176-99-11-185.cloudvps.regruhosting.ru/tonconnect-manifest.json',
-  // manifestUrl: 'https://github.com/tvizzi11k/Zalupa/tonconnect-manifest.json',
 });
-
-tonConnectUI.onStatusChange(console.log)
 
 const unsubscribe = tonConnectUI.onStatusChange(state => {
   window.location.replace('/home.html')
--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,7 +1,15 @@
 Autorization()
-
 function Autorization(){
   if (localStorage.getItem('ton-connect-storage_bridge-connection') === null) {
     window.location.replace('/')
   }
+}
+
+const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
+  manifestUrl: 'https://176-99-11-185.cloudvps.regruhosting.ru/tonconnect-manifest.json',
+});
+
+async function disconect(){
+  await tonConnectUI.disconnect();
+  window.location.replace('/')
 }
